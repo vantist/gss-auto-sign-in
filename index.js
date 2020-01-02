c6b747f9b66b9aba5b2231f5115b4b5e63129753
Vantist <s7790428@gmail.com>
2020-01-02 22:52:08 +0800

    [add] add start script and change port

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function handleEvent(event) {
 }
 
 // listen on port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
